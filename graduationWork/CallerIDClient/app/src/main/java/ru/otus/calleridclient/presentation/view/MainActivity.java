a6319f45ee89f8b17ef3bb6d0ca2fa483fcdca76
Владислав Султаняров <freestyle1448@gmail.com>
2020-02-17 10:52:02 +0300

    Добавил мультимодульность Добавил заготовку под клиент

---
+++ graduationWork/CallerIDClient/app/src/main/java/ru/otus/calleridclient/presentation/view/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.otus.calleridclient.presentation.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ru.otus.calleridclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
